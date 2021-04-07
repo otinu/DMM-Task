c801a1db26b13bccb22bbc04c39e4cf3eb8fe87c
otinu <fitshter@gmail.com>
2021-04-07 12:16:45 +0900

    [Finish]JS-2

--- JavaScript/app.js
+++ JavaScript/app.js
@@ -1,4 +1,19 @@
-var str1 = "Hello";
-var str2 = "World!!";
+var i;
+var count;
+var num = 0;
 
-alert(str1 + str2);
+for(i = 1; i < 100; i++){
+  if (i % 3 == 0) {
+    num = num + i;
+    count = count + 1;
+  }
+  return count;
+}
+
+alert("結果は" + num + "です。" + "加算は" + count + "回繰り返し行われました。");
+
+
+
+
+
+
