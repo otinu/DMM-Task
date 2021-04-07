5df7d9e1898e26f769f8f4f650e10262ed077773
otinu <fitshter@gmail.com>
2021-04-07 17:27:59 +0900

    [Start]JS-3

--- JavaScript/app.js
+++ JavaScript/app.js
@@ -1,19 +1,12 @@
-var i;
-var count;
-var num = 0;
+let alertString;
+alertString = addString("WebCamp");
 
-for(i = 1; i < 100; i++){
-  if (i % 3 == 0) {
-    num = num + i;
-    count = count + 1;
-  }
-  return count;
+alert(alertString);
+
+
+function addString(strA){
+  let addStr = "Hello" + strA;
+  return addStr;
 }
-
-alert("結果は" + num + "です。" + "加算は" + count + "回繰り返し行われました。");
-
-
-
-
 
 
