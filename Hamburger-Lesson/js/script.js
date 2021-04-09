20cedff81a0cef9a9d5e7657d0ff9399943b15d7
otinu <fitshter@gmail.com>
2021-04-09 12:13:36 +0900

    Add-Memo_Hamburger

--- Hamburger-Lesson/js/script.js
+++ Hamburger-Lesson/js/script.js
@@ -1,7 +1,7 @@
 $(function() {
   $(".menu-trigger").on("click", function(event) {
-    $(this).toggleClass("active"); //クリックされたら、activeクラスが追加・削除される。
-    $("#sp-menu").fadeToggle();
+    $(this).toggleClass("active"); //クリックされたら、activeクラスが追加・削除される。 activeクラスは67～74行目前後に記述あり。
+    $("#sp-menu").fadeToggle(500); //第一引数に数字を指定することでフェードする速さが変化する。
     event.preventDefault();
   }); 
 });
